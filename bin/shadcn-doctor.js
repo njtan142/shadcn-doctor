e764275f528e0c2f8e7dfb7e916666631d446fb5
Niño James Tan <njtan142@gmail.com>
2026-03-29 10:48:23 +0800

    fix: pass bail option correctly from bin wrapper

--- bin/shadcn-doctor.js
+++ bin/shadcn-doctor.js
@@ -3,6 +3,6 @@ import { createProgram, run } from '../dist/cli.js';
 
 const program = createProgram();
 program.action(async (path, opts) => {
-  await run(path, opts.format);
+  await run(path, opts.format, { bail: opts.bail });
 });
 program.parse(process.argv);
